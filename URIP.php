9478fac7281b34d97f1cb5bac0b55a158d9e8b08
jeyvpn <62214854+jeyvpn@users.noreply.github.com>
2023-08-28 16:44:49 +0330

    18

---
+++ URIP.php
@@ -0,0 +1,54 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2755
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?php\
+\
+echo "Your IP is";\
+\
+echo $_SERVER["REMOTE_ADDR"];\
+\
+function get_ip_address() \{\
+  // check for shared internet/ISP IP\
+  if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP']))\
+   return $_SERVER['HTTP_CLIENT_IP'];\
+\
+  // check for IPs passing through proxies\
+  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) \{\
+   // check if multiple ips exist in var\
+    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);\
+    foreach ($iplist as $ip) \{\
+     if ($this->validate_ip($ip))\
+      return $ip;\
+    \}\
+   \}\
+\
+  if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))\
+   return $_SERVER['HTTP_X_FORWARDED'];\
+  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))\
+   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];\
+  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))\
+   return $_SERVER['HTTP_FORWARDED_FOR'];\
+  if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))\
+   return $_SERVER['HTTP_FORWARDED'];\
+\
+  // return unreliable ip since all else failed\
+   return $_SERVER['REMOTE_ADDR'];\
+ \}\
+\
+function validate_ip($ip) \{\
+     if (filter_var($ip, FILTER_VALIDATE_IP, \
+                         FILTER_FLAG_IPV4 | \
+                         FILTER_FLAG_IPV6 |\
+                         FILTER_FLAG_NO_PRIV_RANGE | \
+                         FILTER_FLAG_NO_RES_RANGE) === false)\
+         return false;\
+     self::$ip = $ip;\
+     return true;\
+ \}\
+?>}
